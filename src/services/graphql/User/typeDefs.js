9a5dc805d5294e65964c427a6279be5b7b30f1cc
Son Trinh <son.trinh@employmenthero.com>
2019-03-24 17:31:22 +0700

    tweak type defs by adding some more types

--- src/services/graphql/User/typeDefs.js
+++ src/services/graphql/User/typeDefs.js
@@ -1,18 +1,26 @@
 const typeDefs = `
   type User {
     id: Int!
-    email: String!
     password: String!
+    userName: String!
+    firstName: String!
+    lastName: String!
+  }
+
+  type Credential {
+    token: String!
     userName: String!
   }
 
   type Query {
     userName: String!
+    firstName: String!
+    lastName: String!
   }
 
   type Mutation{
-    login(email: String!, password: String!): User!
-    signUp(email: String!, password: String!, userName: String!): User!
+    login(userName: String!, password: String!): Credential!
+    signUp(userName: String!, password: String!, firstName: String!, lastName: String!): User!
   }
 `
 export default typeDefs
