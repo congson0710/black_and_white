bf168a963d18e6842abc441a4efbe5576adc0efe
Son Trinh <son.trinh@employmenthero.com>
2019-03-24 17:31:56 +0700

    delete sync to avoid not creating table everytime  restart server

--- src/services/sequelize/modelRegister.js
+++ src/services/sequelize/modelRegister.js
@@ -10,20 +10,7 @@ AuthorModelCreator(sequelize, Sequelize)
 BookModelCreator(sequelize, Sequelize)
 UserModelCreator(sequelize, Sequelize)
 
-// sequelize.sync({ force: true }).then(() => {
-//   times(() => {
-//     return AuthorModel.create({
-//       first_name: casual.first_name,
-//       last_name: casual.last_name,
-//     }).then(author => {
-//       return author.createBook({
-//         title: casual.title,
-//         cover_image_url: casual.url,
-//         average_rating: casual.integer(10, -10),
-//       })
-//     })
-//   })
-// })
+// sequelize.sync({ force: true })
 
 const Author = sequelize.models.author
 const Book = sequelize.models.book
